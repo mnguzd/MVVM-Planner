235f3d8c899e0c92c49542731c85960d49aa1357
mnguzd <roman.kioroglo@gmail.com>
2020-03-22 12:05:13 +0200

    Removed file

--- ToDoWpf/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.ObjectModel;
-namespace ToDoWpf
-{
-    class Class1
-    {
-        internal static ObservableCollection<Doings> ToDo = new ObservableCollection<Doings>();
-    }
-}
