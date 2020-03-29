9833af1befad0a93fe0807809a5062868f9a473a
mnguzd <roman.kioroglo@gmail.com>
2020-03-29 21:57:39 +0300

    Visual

--- Planner/AppViewModel.cs
+++ Planner/AppViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using Planner.Utilty;
 using Planner.ViewModels;
+using System.Windows;
 
 namespace Planner
 {
     public class AppViewModel : ObservableObject
     {
+        public RelayCommand ClosingWindowCommand { get; private set;}
+        public RelayCommand MinimizeWindowCommand { get; private set; }
         private object _currentView;
         public object CurrentView
         {
@@ -19,8 +22,14 @@ namespace Planner
             set{ if (_tasks == value) return;
                 OnPropertyChanged(ref _tasks, value);}
         }
+        private void MinimizeWindow()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
         public AppViewModel()
         {
+            ClosingWindowCommand = new RelayCommand(p=> Application.Current.Shutdown(), p=>true);
+            MinimizeWindowCommand = new RelayCommand(p => MinimizeWindow(), p => true);
             Tasks = new TasksViewModel();
             CurrentView = Tasks;
         }
