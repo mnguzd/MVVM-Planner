c424bcad884edc1abde7ee5b1a762df7d90e8cee
mnguzd <roman.kioroglo@gmail.com>
2020-04-01 01:30:58 +0300

    Visual Changes

--- Planner/Models/Folder.cs
+++ Planner/Models/Folder.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Planner.Models
 {
     public class Folder
     {
-        public Folder(string str)
-        {
-            Name = str;
-        }
         public Folder(string str, string task)
         {
             Name = str;
             Tasks = new ObservableCollection<Task>();
-            Tasks.Add(new Task(task));
+            for(int i=0;i<Convert.ToInt32(task);i++)
+                Tasks.Add(new Task(task));
         }
         public ObservableCollection<Task> Tasks { get; set; }
         public string Name { get; set; }
